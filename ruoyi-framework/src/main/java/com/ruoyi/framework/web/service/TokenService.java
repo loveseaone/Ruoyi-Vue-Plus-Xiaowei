36e705a8743e7a2cd4037874ec19af6b88ebb0ca
xiaohaibing <xiaohaibing@58yicun.com>
2026-01-08 15:23:35 +0800

    jwt升级

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/TokenService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/TokenService.java
@@ -3,12 +3,19 @@ package com.ruoyi.framework.web.service;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import com.ruoyi.common.constant.CacheConstants;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.domain.model.LoginUser;
@@ -19,9 +26,9 @@ import com.ruoyi.common.utils.ip.AddressUtils;
 import com.ruoyi.common.utils.ip.IpUtils;
 import com.ruoyi.common.utils.uuid.IdUtils;
 import eu.bitwalker.useragentutils.UserAgent;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+
+
+
 
 /**
  * token验证处理
@@ -177,9 +184,10 @@ public class TokenService
      */
     private String createToken(Map<String, Object> claims)
     {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
         String token = Jwts.builder()
                 .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS512, secret).compact();
+                .signWith(key).compact();
         return token;
     }
 
@@ -191,10 +199,12 @@ public class TokenService
      */
     private Claims parseToken(String token)
     {
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
         return Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody();
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     /**
